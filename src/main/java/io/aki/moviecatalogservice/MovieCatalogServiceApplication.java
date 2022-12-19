2ac17301ed7e4b7e21916f58928f163b69e092e6
Akhilesh Yethirajam <33382779+yethirak@users.noreply.github.com>
2022-12-19 18:15:26 +0800

    updated for service timeout

--- src/main/java/io/aki/moviecatalogservice/MovieCatalogServiceApplication.java
+++ src/main/java/io/aki/moviecatalogservice/MovieCatalogServiceApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 //import org.springframework.web.reactive.function.client.WebClient;
 
@@ -15,7 +16,9 @@ public class MovieCatalogServiceApplication {
 	@Bean
 	@LoadBalanced
 	public RestTemplate getRestTemplate(){
-		return new RestTemplate();
+		HttpComponentsClientHttpRequestFactory clientHttpRequestFactory = new HttpComponentsClientHttpRequestFactory();
+		clientHttpRequestFactory.setConnectionRequestTimeout(3000);
+		return new RestTemplate(clientHttpRequestFactory);
 	}
 
 //	@Bean
